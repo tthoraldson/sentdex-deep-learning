90a3e998ea007db2a0f2dc3a1fd695a9b4d4c076
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-10-09 11:49:50 -0500

    part of deep learning intro

---
+++ deep-learning.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+# input > weight > hidden layer 1 (activation function) > weights
+#   > hidden layer 2 (activation function) > weights > output layer
+# ^ an example of a feed forward neural network
+#
+# compare output to intended output > cost function (cross entropy)
+# optimization function (optimizer) > minimize cost (AdamOptimizer, SGD, AdaGrad)
+#
+# backpropagation
+# feed forward + backprop = epoch
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+n_nodes_h13 = 500
+
+n_classes = 10
+batch_size = 100
+
+x = tf.placeholder('float', [None, 784]) # if you don't add [None, 784], an error
+y = tf.placeholder('float')              # will not be thrown if not the same shape
+
+def neural_network_model(data):
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
+                      'biases': tf.Variable(tf.random_normal(n_nodes_hl1))}
